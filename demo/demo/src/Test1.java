18eb46f0ef888e822570bce9a095ce87668bd166
MrChenzhaoyu <mr_chen_czy@163.com>
2021-03-29 23:22:10 +0800

    提交打印0

--- demo/demo/src/Test1.java
+++ demo/demo/src/Test1.java
@@ -1,5 +1,6 @@
 public class Test1 {
     public static void main(String[] args) {
+        System.out.println("test0");
         System.out.println("test1");
         System.out.println("test2");
     }
